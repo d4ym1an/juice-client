cba5b3ec3c3ee5586c602502316a93a81d649279
daymian <lpoco12zz@gmail.com>
2025-02-11 10:15:00 -0800

    .

--- src/preload/splash.js
+++ src/preload/splash.js
@@ -4,10 +4,22 @@ const version = require("../../package.json").version;
 document.addEventListener("DOMContentLoaded", () => {
   const versionElement = document.querySelector(".ver");
   const statusElement = document.querySelector(".status");
+  const splashElement = document.querySelector(".splash");
 
   versionElement.textContent = `v${version}`;
 
   const updateStatus = (status) => (statusElement.textContent = status);
+
+  // List of background images
+  const backgrounds = [
+    "../img/juice_banner1.webp",
+    "../img/juice_banner2.webp",
+    "../img/juice_banner3.webp"
+  ];
+
+  // Select a random background image
+  const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  splashElement.style.backgroundImage = `url(${randomBackground})`;
 
   ipcRenderer.send("check-for-updates");
   updateStatus("Checking for updates...");
